30165763ac76b6053e684722958659aa088ebb91
EC2 Default User <pld9bc@mail.missouri.edu>
2016-05-01 21:37:09 +0000

    main driver implement

--- src/hw6.cpp
+++ src/hw6.cpp
@@ -21,7 +21,6 @@ int main(int argc, char *argv[]){
     if( argc != 3){
         err( "INSUFFICIENT PARAMETERS, format: [int filter size] [input file name] [output file name]" );
     }
-
     std::string input_file    = argv[1];
     std::string output_file   = argv[2];
 
@@ -37,21 +36,44 @@ int main(int argc, char *argv[]){
     uint8_t *output = NULL;
 
     /*
+     * start timer
+     */
+    filter.timerStart();
+    double copy_compute_time = 0;
+
+    /*
      * Push file data to device memory & run sobel filter
      */
+    sobel_filter sobel;
+    copy_compute_time = sobel.gpu_load( &host_image, width, height, &output );
 
     /*
      * verify errors
      */
+    double errors = sobel.cpu_filter_error(&host_image, output, width, height)
+    if( errors < 0 ){
+        err( "ERROR ON CPU IMAGE CREATE" );
+    }
 
+    /*
+     * save gpu-produced image
+     */
+    if(sdkSavePGM( output_file.c_str(), output, width, height ) == false){
+        err( "ERROR ON PPM save" );
+    }
 
     /*
      * Print timings
      */
+    double total_time = sobel.getTime();
+    std::cout << "Copy compute time: " << copy_compute_time << " ms" << std::endl;
+    std::cout << "Total time: " << total_time << " ms" << std::endl;
+    std::cout << "Error percentage: " << errors << std::endl;
 
     /*
      * Cleanup
      */
+    filter.timerStop();
     free(host_image);
     free(output);
     return 0;
